db4ea0dca11e440aaf5ffdb79382b3ea8f2c9768
ag1479 <itsashikgurung@gmail.com>
2020-04-25 18:18:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Caching the DOM
 
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
